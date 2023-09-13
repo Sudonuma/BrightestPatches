ceefec19c62477b86d8d0881548617c3781c06cc
Sudonuma <tasnim.hd@gmail.com>
2023-09-13 14:51:01 +0200

    fix the test of read_image

--- tests/test_read_image.py
+++ tests/test_read_image.py
@@ -5,7 +5,7 @@ import pytest
 from brightest_patch import read_image
 
 def test_read_image_color() -> None:
-    image_path = './img.jpg'
+    image_path = './img.png'
     img = read_image(image_path)
 
     assert isinstance(img, np.ndarray)
@@ -14,7 +14,7 @@ def test_read_image_color() -> None:
     assert img.shape[1]  > 0
 
 def test_read_image_gray() -> None:
-    image_path = './img.jpg'
+    image_path = './img.png'
     img = read_image(image_path, grayscale=True)
 
     assert isinstance(img, np.ndarray)
