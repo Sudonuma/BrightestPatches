977251b06153f6fb6fd82ac9163f182a1fc21b38
Sudonuma <tasnim.hd@gmail.com>
2023-10-03 17:28:43 +0200

    add fastapi

--- frontend/brightest_patches.js
+++ frontend/brightest_patches.js
@@ -2,6 +2,12 @@
 const uploadButton = document.getElementById("upload-button");
 const fileInput = document.getElementById("file-input");
 const uploadedImage = document.getElementById("uploaded-image");
+const results = document.getElementById("results");
+const resultImage = document.getElementById("result-image");
+const showResults = document.getElementById("show-results");
+// set result path to empty image at first
+let resultPath = ''
+
 
 // Add a click event listener to the button
 uploadButton.addEventListener("click", () => {
@@ -12,6 +18,7 @@ uploadButton.addEventListener("click", () => {
 // Add an event listener to the file input
 fileInput.addEventListener("change", () => {
     // Check if a file is selected
+// show image in html page
     if (fileInput.files.length > 0) {
         const file = fileInput.files[0];
         // console.log(fileInput.files)
@@ -29,5 +36,50 @@ fileInput.addEventListener("change", () => {
         console.log(file)
         // Read the selected file as a data URL (image)
         reader.readAsDataURL(file);
+    
+    // send image to fastapi and get results
+    if (file) {
+        const formData = new FormData();
+        formData.append("file", file);
+
+        fetch("http://localhost:8000/process-image", {
+            method: "POST",
+            body: formData,
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response from the backend (e.g., display results)
+            resultPath = data["output_path"]
+            console.log(data);
+        })
+        .catch(error => {
+            // Handle errors (e.g., display an error message)
+            console.error(error);
+        });
+    }}
+});
+
+
+// // Add an event listener to the results: if it is clicked show results
+// document.addEventListener('DOMContentLoaded', () => {
+
+//     results.addEventListener("click", () => {
+//             // Set the image source to the loaded file data URL
+//             resultImage.src = resultPath;
+
+//             // Display the uploaded image
+//             resultImage.style.display = "block";
+        
+//     })});
+
+document.body.addEventListener("click", (event) => {
+    // Check if the clicked element has the ID "results"
+    if (event.target.id === "results") {
+        // Set the image source to the loaded file data URL
+        resultImage.src = resultPath;
+        // console.log(resultPath)
+
+        // Display the uploaded image
+        resultImage.style.display = "block";
     }
 });
