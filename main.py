de1ad673021a11f8b30415eaeca43f6d80f42337
Tesnim <tasnim.hd@gmail.com>
2023-10-03 17:31:44 +0200

    add fastapi (#2)

--- main.py
+++ main.py
@@ -1,39 +1,78 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 import cv2
+import os
 import numpy as np
 import brightest_patch
-import argparse
-import os
+from fastapi.staticfiles import StaticFiles
+import time
 
-def main():
-    
-    parser = argparse.ArgumentParser(description='Brightest partches in the image.')
+app = FastAPI()
 
-    # Add an argument for the input image path
-    default_image = './img.png'
-    parser.add_argument('--image_path', type=str, default=default_image, help=f'Path to the image (default: {default_image})')
 
-    # Add an argument for the output image path with the default set to the root of the project
-    default_output_path = os.path.join(os.getcwd(), 'output.png')
-    parser.add_argument('--output_path', type=str, default=default_output_path, help=f'Path to the output image (default: {default_output_path})')
 
-    # Parse the command-line arguments
-    args = parser.parse_args()
-    
+origins = [
+    '*',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Define the path to the "static" folder where you want to save the images
+static_folder = os.path.join(os.path.dirname(__file__), "static")
+
+# Ensure the "static" folder exists, create it if necessary
+os.makedirs(static_folder, exist_ok=True)
+
+app.mount("/static", StaticFiles(directory=static_folder), name="static")
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
+
+
+@app.post("/process-image")
+async def upload_image(file: UploadFile):
+    # Process the uploaded image (replace with your processing logic)
+    # timestr = time.strftime("%Y%m%d-%H%M%S")
+    if not file:
+        raise HTTPException(status_code=400, detail="No image provided")
+
+    # Save the uploaded image to the "static" folder
+    image_path = os.path.join(static_folder, file.filename)
+    with open(image_path, "wb") as image_file:
+        image_file.write(file.file.read())
+
+
     patches = []
     patch_coordinates = [] 
     patch_mean_intensities = []
 
-    brightest_patch.extract_patches(args.image_path, patches, patch_coordinates, patch_mean_intensities)
+    brightest_patch.extract_patches(image_path, patches, patch_coordinates, patch_mean_intensities)
+
+    # Return the computed results as JSON response
+    
     brightest_four, brightest_patches_coordinates, mean_brightness = brightest_patch.four_brightest_patches(patches, patch_coordinates, patch_mean_intensities)
 
     corners = brightest_patch.patch_centers(brightest_patches_coordinates)
 
     area = brightest_patch.compute_area(corners)
-    brightest_patch.draw_area(args.image_path, corners, args.output_path)
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    
+    output_basename = os.path.splitext(os.path.basename(image_path))[0]
+    output_path = os.path.join(static_folder, output_basename+timestr+".png")
+    brightest_patch.draw_area(image_path, corners, output_path)
 
     print(f'the area of the quadrilateral which the corners are the centers of the brightest patches of the image is: {area} pixels')
 
-    
+    return {"output_path": output_path, "message": f'the area of the quadrilateral which the corners are the centers of the brightest patches of the image is: {area} pixels'}
 
-if __name__ == "__main__":
-    main()
+# favicon.ico
+# timestamp for the input too? (yes to solve problems with input with the same name)
+
